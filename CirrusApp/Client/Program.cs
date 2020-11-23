0743aa53f8786cc20855407d80341b64dae8b7f6
Daniel Åslund <daniel.aaslund@gmail.com>
2020-11-23 21:13:18 +0100

    Added State Container

--- CirrusApp/Client/Program.cs
+++ CirrusApp/Client/Program.cs
@@ -17,7 +17,9 @@ namespace CirrusApp.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services
+                .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddSingleton<StateContainer>();
 
             await builder.Build().RunAsync();
         }
---
+++ CirrusApp/Client/StateContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CirrusApp.Client
+{
+    public class StateContainer
+    {
+        public bool IsAuthenticated { get; set; } = false;
+
+        public event Action onChange;
+
+        public void SetProperty()
+        {
+            IsAuthenticated = !IsAuthenticated;
+        }
+
+        private void NotifyStateChanged() => onChange?.Invoke();
+    }
+}
