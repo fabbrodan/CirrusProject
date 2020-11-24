837e22c3cfaae845e6f4ca063b7bbab653bb7dd6
Daniel Åslund <fabbrodan@users.noreply.github.com>
2020-11-24 22:08:06 +0100

    Removing Endpoint data from Cosmos Db Service

--- cirrus-functions/cirrus-functions/CosmosDbService.cs
+++ cirrus-functions/cirrus-functions/CosmosDbService.cs
@@ -4,10 +4,10 @@ namespace cirrus_functions
 {
     public class CosmosDbService
     {
-        private string CosmosURI = "https://cirrus-csp.documents.azure.com:443/";
-        private string CosmosKey = "VV4xsp3QkDSnqaDllWIpblTo9uJjybNkDHlAzqGhDwA6YUC4TdOHsCQuPkwgU6shIQl4Lfu6s7N844WUQXVEVw==";
-        private string CosmosDB = "cirrus-db";
-        private string CosmosContainerName = "cirrus-container";
+        private string CosmosURI = "";
+        private string CosmosKey = "";
+        private string CosmosDB = "";
+        private string CosmosContainerName = "";
 
         private CosmosClient CosmosDbClient;
         public Container CosmosContainer;
